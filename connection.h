87673a282389e616c302ea783f3f24a7332c311a
Atmanuclear <740414270@qq.com>
2018-06-16 10:05:33 +0800

    1.0
    数据库基本操作和显示

---
+++ connection.h
@@ -0,0 +1,24 @@
+#ifndef CONNECTION_H
+#define CONNECTION_H
+
+#include <QtSql>
+#include <QSqlQuery>
+
+static bool createConnection()
+{
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+        db.setHostName("localhost");
+        db.setPort(3306);
+        db.setDatabaseName("qtest");
+        db.setUserName("root");
+        db.setPassword("123456");
+    if(!db.open()) return false;
+    QSqlQuery query;
+    query.exec("create table student (id int primary key, name vchar)");
+    query.exec("insert into student values (0,'刘明')");
+    query.exec("insert into student values (1,'陈刚')");
+    query.exec("insert into student values (2,'王红')");
+    return true;
+}
+
+#endif // CONNECTION_H
---
+++ mainwindow.cpp
@@ -0,0 +1,131 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QDebug>
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    model = new QSqlTableModel(this);
+    model2 = new QSqlTableModel(this);
+    model3 = new QSqlQueryModel(this);
+    model->setTable("student");
+    model->setEditStrategy(QSqlTableModel::OnManualSubmit);
+    model->select(); //选取整个表的所有行
+
+    //不显示name属性列,如果这时添加记录，则该属性的值添加不上
+    // model->removeColumn(1);
+
+   // ui->tableView->setModel(model);
+
+    //使其不可编辑
+    //ui->tableView->setEditTriggers(QAbstractItemView::NoEditTriggers);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    model->database().transaction(); //开始事务操作
+    if (model->submitAll()) {
+        model->database().commit(); //提交
+    } else {
+        model->database().rollback(); //回滚
+        QMessageBox::warning(this, tr("tableModel"),
+                             tr("数据库错误: %1")
+                             .arg(model->lastError().text()));
+    }
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    model->revertAll();
+}
+
+void MainWindow::on_pushButton_5_clicked()
+{
+    QString name = ui->lineEdit->text();
+    //根据姓名进行筛选
+    model->setFilter(QString("name = '%1'").arg(name));
+    //显示结果
+    model->select();
+}
+
+void MainWindow::on_pushButton_6_clicked()
+{
+    model->setTable("student");   //重新关联表
+    model->select();
+    ui->tableView->setModel(model);
+    //这样才能再次显示整个表的内容
+}
+
+// 升序
+void MainWindow::on_pushButton_7_clicked()
+{
+    //id属性即第0列，升序排列
+    model->setSort(0, Qt::AscendingOrder);
+    model->select();
+}
+// 降序
+void MainWindow::on_pushButton_8_clicked()
+{
+    model->setSort(0, Qt::DescendingOrder);
+    model->select();
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    //获取选中的行
+    int curRow = ui->tableView->currentIndex().row();
+
+    //删除该行
+    model->removeRow(curRow);
+
+    int ok = QMessageBox::warning(this,tr("删除当前行!"),
+                                  tr("你确定删除当前行吗？"),
+                                  QMessageBox::Yes,QMessageBox::No);
+    if(ok == QMessageBox::No)
+    {
+        model->revertAll(); //如果不删除，则撤销
+    }
+    else model->submitAll(); //否则提交，在数据库中删除该行
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    int rowNum = model->rowCount(); //获得表的行数
+    int id = 10;
+    model->insertRow(rowNum); //添加一行
+    model->setData(model->index(rowNum,0),id);
+    //model->submitAll(); //可以直接提交
+}
+
+void MainWindow::on_pushButton_9_clicked()
+{
+    model2->setTable("teachers");   //重新关联表
+    model2->select();
+    ui->tableView_2->setModel(model2);
+}
+
+void MainWindow::on_pushButton_10_clicked()
+{
+    QSqlQuery query = model3->query();
+
+    query.exec("insert name from teachers where tcid = 123123");
+    QString tname = ui->lineEdit_2->text();
+
+    model3->setQuery("select tname from teachers where tcid = 123123 ");
+
+    ui->tableView_2->setModel(model3);
+
+}
